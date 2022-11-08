08f336588201f9cde5189f03d6fb1a5289d7c272
uge <chaitanyasuresh.uge@infineon.com>
2022-11-08 20:29:13 +0530

    herosection added

--- src/containers/heroSection/HeroSection.jsx
+++ src/containers/heroSection/HeroSection.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './heroSection.css';
+import ai from '../../assets/ai.png';
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className="cisco__herosection section__padding" id="home">
+      <div className="cisco__herosection-content">
+        <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 OpenAI</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book</p>
+      </div>
+
+      <div className='cisco__herosection-image'>
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
   )
 }
 
