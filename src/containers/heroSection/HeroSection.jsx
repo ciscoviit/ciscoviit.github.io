0ec1aa05dfb5bb2665684df84921498e496e363c
pratikrk <83352530+pratikrk@users.noreply.github.com>
2022-11-23 20:10:10 +0530

    added fancy fonts

--- src/containers/heroSection/HeroSection.jsx
+++ src/containers/heroSection/HeroSection.jsx
@@ -4,34 +4,34 @@ import './heroSection.css';
 
 const Header = () => (
   <>
-  <div className='gradient__bg-home'>
+    <div className='gradient__bg-home'>
 
-    <div className="cisco__header section__padding" id="home">
-      <div className="cisco__header-content">
-        <h1 className="main-heading">
-          Welcome to <span>Cisco ViiT</span>
-          <br />
-          Connect Everywhere
-        </h1>
-        <p className="heading-fancy-font text-center">
-          Loved by Developers, Trusted By Bussiness.
-        </p>
-        
+      <div className="cisco__header section__padding" id="home">
+        <div className="cisco__header-content">
+          <h1 className="main-heading">
+            Welcome to <span>Cisco ViiT</span>
+            <br />
+            Connect Everywhere
+          </h1>
+          <p className="heading-fancy-font">
+            Loved by Developers, Trusted By Bussiness.
+          </p>
+
+        </div>
+        <div className="cisco__header-image">
+          <img alt='' src="https://images1.the-dots.com/1247540/spacegif4.gif?p=projectImageFullJpg" />
+        </div>
       </div>
-      <div className="cisco__header-image">
-        <img alt='' src="https://images1.the-dots.com/1247540/spacegif4.gif?p=projectImageFullJpg" />
-      </div>
-    </div>
-    <div className='cisco__header section__padding_btn'>
-    <div className="btn_wrapper">
-        <NavLink to='/about'><button className="btn view_more_btn">
+      <div className='cisco__header section__padding_btn'>
+        <div className="btn_wrapper">
+          <NavLink to='/about'><button className="btn view_more_btn">
             About <i className="ri-arrow-right-line"></i>
           </button></NavLink>
 
           <NavLink to='courses'><button className="btn documentation_btn">Courses</button></NavLink>
         </div>
+      </div>
     </div>
-  </div>
   </>
 );
 
