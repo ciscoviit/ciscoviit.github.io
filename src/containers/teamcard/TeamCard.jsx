a58376322eed7a849e7a7e83efa102ad4ba090f5
Amol Sominath Golhar <117825386+AmolSGolhar@users.noreply.github.com>
2023-10-31 21:58:48 +0530

    Update TeamCard.jsx

--- src/containers/teamcard/TeamCard.jsx
+++ src/containers/teamcard/TeamCard.jsx
@@ -96,10 +96,10 @@ const TeamCard = ()=>{
 
                 <div className="row justify-content-center">
                     <div className="col-md-6 col-lg-4 mb-5">
-                        <TeamSection Tag={<i className="fas fa-code"></i>} image={Akash_Kalshetti} Name="Shreyas Gavhalkar" domain="President" linkedin="https://www.linkedin.com/in/shreyas-gavhalkar-a8b0bb206"  instagram="www.instagram.com/__shreyas__19"/>
+                        <TeamSection Tag={<i className="fas fa-code"></i>} image={Akash_Kalshetti} Name="Akash Kalshetti" domain="President" linkedin="https://www.linkedin.com/in/shreyas-gavhalkar-a8b0bb206"  instagram="www.instagram.com/__shreyas__19"/>
                     </div>
                     <div className="col-md-6 col-lg-4 mb-5">
-                        <TeamSection Tag={<i className="fas fa-code"></i>} image={Sachi_Teke} Name="Sachi Teke" domain="Vice-President" linkedin="https://www.linkedin.com/in/sachi-teke-118469206/" twitter="https://twitter.com/sachi_teke" instagram="https://instagram.com/sachi_teke7032"/>
+                        <TeamSection Tag={<i className="fas fa-code"></i>} image={Sumedh_kulkarni} Name="Sumedh_kulkarni" domain="Vice-President" linkedin="https://www.linkedin.com/in/sachi-teke-118469206/" twitter="https://twitter.com/sachi_teke" instagram="https://instagram.com/sachi_teke7032"/>
                     </div>
                     <div className="col-md-6 col-lg-4 mb-5">
                         <TeamSection Tag={<i className="fas fa-code"></i>} image={Pratik_Rathod} Name="Pratik Rathod" domain="Publicity-Head" linkedin="https://www.linkedin.com/in/pratik-rathod-10a522208" twitter="https://twitter.com/cyph3r_s" instagram="https://www.instagram.com/pratik_rathod_1/"/>
