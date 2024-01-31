afaca2ac01918bb52eff01900a7b47a18ee4785c
hrishikesh-mahajan <hrishikeshvmahajan@gmail.com>
2024-01-31 21:37:58 +0530

    Add new photos

--- src/containers/teamcard/TeamCard.jsx
+++ src/containers/teamcard/TeamCard.jsx
@@ -5,22 +5,47 @@ import "./TeamCard.css";
 // import Carousel from "react-elastic-carousel";
 import "react-multi-carousel/lib/styles.css";
 import FooterHeart from "../footerHeart/FooterHeart";
+
+// Importing Images
+// Faculty Co-ordinators
 import Nitin_Sakhare from "../../assets/TeamDocuments/Nitin_Sakhare.jpg";
 import Vikas_Kolekar from "../../assets/TeamDocuments/Vikas_Kolekar.jpg";
+// Advisory Board
+import Saikumar_Padmawar from "../../assets/TeamDocuments/Saikumar_Padmawar.jpg";
+import Vinayak_Pawar from "../../assets/TeamDocuments/Vinayak_Pawar.jpg";
+// TY Committee
+import Akash_Kalshetti from "../../assets/TeamDocuments/Akash_Kalshetti.jpg";
+import Sumedh_kulkarni from "../../assets/TeamDocuments/Sumedh_kulkarni.jpg";
+import Sanchit_Dhale from "../../assets/TeamDocuments/Sanchit_Dhale.jpg";
+import Mayur_Tingare from "../../assets/TeamDocuments/Mayur_Tingare.jpg";
+import Hrishikesh_Mahajan from "../../assets/TeamDocuments/Hrishikesh_Mahajan.jpg";
+import Amol_Golhar from "../../assets/TeamDocuments/Amol_Golhar.jpg";
+import Nikita_Sanap from "../../assets/TeamDocuments/Nikita_Sanap.jpg";
+import Rahul_Shelke from "../../assets/TeamDocuments/Rahul_Shelke.jpg";
+import Khushi_Asodekar from "../..//assets/TeamDocuments/Khushi_Asodekar.jpg";
+import Shruti_Ghodake from "../../assets/TeamDocuments/Shruti_Ghodake.jpg";
+import Sahil_Wagh from "../../assets/TeamDocuments/Sahil_Wagh.jpg";
+import Deepak_Ghume from "../../assets/TeamDocuments/Deepak_Ghume.jpg";
+import Rajan_Sudan from "../../assets/TeamDocuments/Rajan_Sudan.jpg";
+import Devyani_Dahake from "../../assets/TeamDocuments/Devyani_Dahake.jpg";
+import Smit_Ramteke from "../../assets/TeamDocuments/Smit_Ramteke.jpg";
+import Vedant_Devre from "../../assets/TeamDocuments/Vedant_Devre.jpg";
+import Viraj_Badale from "../../assets/TeamDocuments/Viraj_Badale.jpg";
+import Ashish_Ruke from "../../assets/TeamDocuments/Ashish_Ruke.jpg";
+
+// Previous Year Committees
 // import Shreyas_Gavhalkar from "../../assets/TeamDocuments/Shreyas_Gavhalkar.jpg";
 // import Sachi_Teke from "../../assets/TeamDocuments/Sachi_Teke.jpg";
 // import Pratik_Rathod from "../../assets/TeamDocuments/Pratik_Rathod.jpg";
 // import Tejaswini_Bahir from "../../assets/TeamDocuments/Tejaswini_Bahir.jpg";
-import Saikumar_Padmawar from "../../assets/TeamDocuments/Saikumar_Padmawar.jpg";
 // import Shrutika_Wagh from "../../assets/TeamDocuments/Shrutika_Wagh.jpg";
 // import Vishnu_Jadhav from "../../assets/TeamDocuments/Vishnu_Jadhav.jpg";
 // import Sayali_Sontakke from "../../assets/TeamDocuments/Sayali_Sontakke.jpg";
-// import Nikhilesh_Sharma from "../../assets/TeamDocuments/Nikhilesh_Sharma.jpg";
 // import Samruddhi_Nande from "../../assets/TeamDocuments/Samruddhi_Nande.jpg";
+// import Nikhilesh_Sharma from "../../assets/TeamDocuments/Nikhilesh_Sharma.jpg";
 // import Yash_Akotkar from "../../assets/TeamDocuments/Yash_Akotkar.jpg";
 // import Siddharth_Latthe from "../../assets/TeamDocuments/Siddharth_Latthe.jpg";
 // import Rahul_Talele from "../../assets/TeamDocuments/Rahul_Talele.jpg";
-import Vinayak_Pawar from "../../assets/TeamDocuments/Vinayak_Pawar.jpg";
 // import Shravani_Murabatte from "../../assets/TeamDocuments/Shravani_Murabatte.jpg";
 // import Shubham_Wamanacharya from "../../assets/TeamDocuments/Shubham_Wamanacharya .jpg";
 // import Amey_Agrawal from "../../assets/TeamDocuments/Amey_Agrawal.jpg";
@@ -30,35 +55,19 @@ import Vinayak_Pawar from "../../assets/TeamDocuments/Vinayak_Pawar.jpg";
 // import Armaan_Shaikh from "../../assets/TeamDocuments/Armaan_Shaikh.jpg";
 // import Riya_Agarwal from "../../assets/TeamDocuments/Riya_Agarwal.jpg";
 // import Abhiskek_Jadhav from "../../assets/TeamDocuments/Abhishek_Jadhav.jpg";
-// import Mayur_Tingare from "../../assets/TeamDocuments/Mayur_Tingare.jpg";
 // import Yash_Patil from "../../assets/TeamDocuments/Yash_Patil.jpg";
-import Sahil_Wagh from "../../assets/TeamDocuments/Sahil_Wagh.jpg";
-import Rahul_Shelke from "../../assets/TeamDocuments/Rahul_Shelke.jpg";
 // import Prathamesh_Bhaskar from "../../assets/TeamDocuments/Prathamesh_Bhaskar.jpg";
 // import Arnav_Umarkar from "../../assets/TeamDocuments/Arnav_Umarkar.jpg";
-import Vedant_Devre from "../../assets/TeamDocuments/Vedant_Devre.jpg";
 // import Sankalp_Akkewar from "../../assets/TeamDocuments/Sankalp_Akkewar.jpg";
 // import Hasan_Hussain from "../../assets/TeamDocuments/Hasan_Hussain.jpg";
-import Akash_Kalshetti from "../../assets/TeamDocuments/Akash_Kalshetti.jpg";
 // import Manas_Sangale from "../../assets/TeamDocuments/Manas_Sangale.jpg";
-// import Sanchit_Dhale from "../../assets/TeamDocuments/Sanchit_Dhale.jpg";
 // import Gunesh_Sonawane from "../../assets/TeamDocuments/Gunesh_Sonawane.jpg";
 // import Siddhesh_Khairnar from "../../assets/TeamDocuments/Siddhesh_Khairnar.jpg";
 // import HARSH from "../../assets/TeamDocuments/HARSH.jpg";
 // import Om_Dhede from "../../assets/TeamDocuments/Om_Dhede.jpg";
 // import Chaitanya_Ganjiwale from "../../assets/TeamDocuments/Chaitanya_Ganjiwale.jpg";
 // import Ankush_Singh from "../../assets/TeamDocuments/Ankush_Singh.jpg";
-import Smit_Ramteke from "../../assets/TeamDocuments/Smit_Ramteke.jpg";
 // import Sandesh_Buchkul from "../../assets/TeamDocuments/Sandesh_Buchkul.jpg";
-import Sumedh_kulkarni from "../../assets/TeamDocuments/Sumedh_kulkarni.jpg";
-import Viraj_Badale from "../../assets/TeamDocuments/Viraj_Badale.jpg";
-import Ashish_Ruke from "../../assets/TeamDocuments/Ashish_Ruke.jpg";
-import Amol_Golhar from "../../assets/TeamDocuments/Amol_Golhar.jpg";
-import Deepak_Ghume from "../../assets/TeamDocuments/Deepak_Ghume.jpg";
-import Devyani_Dahake from "../../assets/TeamDocuments/Devyani_Dahake.jpg";
-import Nikita_Sanap from "../../assets/TeamDocuments/Nikita_Sanap.jpg";
-import Shruti_Ghodake from "../../assets/TeamDocuments/Shruti_Ghodake.jpg";
-import Hrishikesh_Mahajan from "../../assets/TeamDocuments/Hrishikesh_Mahajan.jpg";
 
 // const breakPoints = [
 //   { width: 1, itemsToShow: 1, itemsToScroll: 1 },
