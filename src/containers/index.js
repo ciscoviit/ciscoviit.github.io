fc0bebd4003cc5bd41f78f92ce02900d32c265bd
Yash Pawar <yashpawarp@gmail.com>
2025-01-13 22:33:32 +0530

    remove old commits

--- src/containers/index.js
+++
@@ -1,9 +0,0 @@
-export { default as ContactPage } from "./contact/ContactPage";
-export { default as CourseCard } from "./coursecard/CourseCard";
-export { default as Features } from "./features/Features";
-export { default as Footer } from "./footer/Footer";
-export { default as FooterHeart } from "./footerHeart/FooterHeart";
-export { default as Gallery } from "./gallery/Gallery";
-export { default as HeroSection } from "./heroSection/HeroSection";
-export { default as TeamSection } from "./team/TeamSection";
-// export { default as Timeline } from './timeline/Timeline'
